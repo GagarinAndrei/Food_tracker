dc49d1c5b358001ab54a6fd225a3bfd0bce88f2b
GagarinAndrei <gagarin1982@gmail.com>
2022-09-26 16:01:54 +0500

    add food data class

---
+++ app/src/main/java/ru/gagarin/foodtracker/foodDetails.kt
@@ -0,0 +1,12 @@
+package ru.gagarin.foodtracker
+
+data class foodDetails(
+    val id: Int,
+    val name: String,
+    val calories: Int,
+    val carbs: Int,
+    val protein: Int,
+    val fat: Int
+) {
+
+}
